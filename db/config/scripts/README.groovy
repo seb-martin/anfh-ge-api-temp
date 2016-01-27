21511f0a0a4454b4a455990a626e916f3e48ca77
Sébastien Martin <seb.martin.94@gmail.com>
2016-01-27 23:17:13 +0100

    chore: Impose à Git le dossier scripts de la configuration de elasticsearch

---
+++ db/config/scripts/README.groovy
@@ -0,0 +1,7 @@
+
+/*
+ * - La présence du répertoire `scripts` dans le dossier de configuration est requis par elasticsearch,
+ * - mais Git n'indexe pas les dossiers vides
+ *
+ * La présence de ce fichier impose à Git la présence du dossier `scripts`.
+ */
