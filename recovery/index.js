ff8e401717894b9b413bf4dc98738a58aa29c865
Sébastien Martin <seb.martin.94@gmail.com>
2016-02-26 14:48:48 +0100

    pas de reprise du contexte et des objectifs dans le module
    Ne recopie pas les champs contexte et objectifs de l'action au niveau du module
    
    Closes #24

--- recovery/index.js
+++ recovery/index.js
@@ -166,15 +166,15 @@
         --> On réplique des infos de l'action :
             - intitulé du module = intitulé de l'action
             - Formateur module = Titulaire de l'action
-            - Contexte module = contexte action
-            - Objectifs module = objectifs action
+            - /!\ NON (Issue #24) : Contexte module = contexte action
+            - /!\ NON (Issue #24) : Objectifs module = objectifs action
             - Publics module = publics action
         --> On reprend les données programme et durée ensuite
         */
         srctgt.target.action.modules = [{
           intitule: srctgt.target.action.intitule,
-          contexte: srctgt.target.action.contexte,
-          objectifs: srctgt.target.action.objectifs,
+          // contexte: srctgt.target.action.contexte,
+          // objectifs: srctgt.target.action.objectifs,
           formateur: srctgt.target.action.titulaire,
           publics: srctgt.target.action.publics.map(function(p) {
             // Réalise une copie des éléments du tableau
